8befb144b3e41c9376d0790f2e3766337ddf0386
Rakeshpython1999 <151004444+Rakeshpython1999@users.noreply.github.com>
2025-10-16 12:42:21 +0530

    Crud app Completed

--- crud_app/urls.py
+++ crud_app/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
+    path('delete/<int:id>/', views.delete_student, name='delete_student'),
+    path('add/', views.add_student, name='add_student'),
+    path('update/<int:id>/', views.update_student, name='update_student'), 
 ]
--- crud_app/views.py
+++ crud_app/views.py
@@ -1,8 +1,41 @@
-from django.shortcuts import render
+from pyexpat.errors import messages
+from sqlite3 import IntegrityError
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .models import Student
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     student = Student.objects.all()
     return render(request, 'index.html', {'students' : student})
+
+
+def delete_student(request , id):
+    student = Student.objects.get(id=id)
+    student.delete()
+    return redirect('home')
+
+def add_student(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        age = request.POST.get('age')
+
+        if Student.objects.filter(email=email).exists():
+            messages.error(request, "Email already exists! Please use a different email.")
+            return redirect('home')
+
+        Student.objects.create(name=name, email=email, age=age)
+        messages.success(request, "Student added successfully!")
+        return redirect('home')
+
+def update_student(request, id):
+    student = Student.objects.get(id=id)
+    if request.method == "POST":
+        student.name = request.POST['name']
+        student.email = request.POST['email']
+        student.age = request.POST['age']
+        student.save()
+        return redirect('home')
+    return render(request, 'home.html', {'student': student})
